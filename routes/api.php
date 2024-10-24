80e2f93b0357ad5632df1deed2ad606c7d3ec9c4
myrdinaptaliesen <nicog.dev@gmail.com>
2024-10-24 16:38:28 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\ClubController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\API\PlayerController; 
